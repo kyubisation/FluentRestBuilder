130f5bfd292034a7de53709b85652da9c5c19e05
Lukas Spirig <lukas.spirig@gmail.com>
2016-12-10 23:14:48 +0100

    Created accepted result pipe

---
+++ src/FluentRest.Core/Results/AcceptedResult/AcceptedResultPipe.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="AcceptedResultPipe.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRest.Core.Results.AcceptedResult
+{
+    using Common;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AcceptedResultPipe<TInput> : ResultPipe<TInput>
+        where TInput : class
+    {
+        public AcceptedResultPipe(IOutputPipe<TInput> parent)
+            : base(parent)
+        {
+        }
+
+        protected override IActionResult CreateResult(TInput source) =>
+            new ObjectResult(source) { StatusCode = StatusCodes.Status202Accepted };
+    }
+}
---
+++ src/FluentRest.Core/Results/AcceptedResult/Integration.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Integration.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+// ReSharper disable once CheckNamespace
+namespace FluentRest
+{
+    using System.Threading.Tasks;
+    using Core;
+    using Core.Results.AcceptedResult;
+    using Microsoft.AspNetCore.Mvc;
+
+    public static partial class Integration
+    {
+        public static Task<IActionResult> ToAcceptedResult<TInput>(
+            this IOutputPipe<TInput> pipe)
+            where TInput : class
+        {
+            IPipe resultPipe = new AcceptedResultPipe<TInput>(pipe);
+            return resultPipe.Execute();
+        }
+    }
+}
