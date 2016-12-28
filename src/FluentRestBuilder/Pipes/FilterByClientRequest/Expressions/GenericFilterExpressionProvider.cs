c2184612b12d040d2eed701249ca240321fceade
Lukas Spirig <lukas.spirig@gmail.com>
2016-12-29 00:03:45 +0100

    Implemented filter expression provider

---
+++ src/FluentRestBuilder/Pipes/FilterByClientRequest/Expressions/GenericFilterExpressionProvider.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="GenericFilterExpressionProvider.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Pipes.FilterByClientRequest.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    public class GenericFilterExpressionProvider<TEntity, TFilter> : FilterExpressionProvider<TEntity, TFilter>
+    {
+        private readonly Func<string, TFilter> conversion;
+
+        public GenericFilterExpressionProvider(
+            IDictionary<FilterType, Func<TFilter, Expression<Func<TEntity, bool>>>> filterDictionary,
+            Func<string, TFilter> conversion)
+            : base(filterDictionary)
+        {
+            this.conversion = conversion ?? ConvertToFilterType;
+        }
+
+        public override Expression<Func<TEntity, bool>> Resolve(FilterType type, string filter)
+        {
+            try
+            {
+                var filterValue = this.conversion(filter);
+                return this.ResolveForType(type, filterValue);
+            }
+            catch (Exception)
+            {
+                return e => false;
+            }
+        }
+
+        private static TFilter ConvertToFilterType(string filter) =>
+            (TFilter)Convert.ChangeType(filter, typeof(TFilter));
+    }
+}
---
+++ src/FluentRestBuilder/Pipes/FilterByClientRequest/Expressions/StringFilterExpressionProvider.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="StringFilterExpressionProvider.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Pipes.FilterByClientRequest.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    public class StringFilterExpressionProvider<TEntity> : FilterExpressionProvider<TEntity, string>
+    {
+        public StringFilterExpressionProvider(
+            IDictionary<FilterType, Func<string, Expression<Func<TEntity, bool>>>> filterDictionary)
+            : base(filterDictionary)
+        {
+        }
+
+        public override Expression<Func<TEntity, bool>> Resolve(FilterType type, string filter) =>
+            this.ResolveForType(type, filter);
+    }
+}
