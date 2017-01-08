15852d49c1fc218b5eb37b2786136776f866795e
Lukas Spirig <lukas.spirig@gmail.com>
2017-01-08 21:48:28 +0100

    Created builder extension

---
+++ src/FluentRestBuilder.Caching/Builder/FluentRestBuilderExtension.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="FluentRestBuilderExtension.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+// ReSharper disable once CheckNamespace
+namespace FluentRestBuilder
+{
+    using Builder;
+
+    public static class FluentRestBuilderExtension
+    {
+        public static IFluentRestBuilder AddCachingPipes(
+            this IFluentRestBuilder builder)
+        {
+            return builder
+                .RegisterInputMemoryCachePipe()
+                .RegisterInputDistributedCachePipe()
+                .RegisterActionResultMemoryCachePipe()
+                .RegisterActionResultDistributedCachePipe();
+        }
+    }
+}
