d5878ecde3d849c0f047e5ecfbfe2585be38d412
Lukas Spirig <lukas.spirig@gmail.com>
2017-01-13 14:47:29 +0100

    Implemented unit tests for queryable transformers

---
+++ test/FluentRestBuilder.Test.Common/Mocks/PersistantDatabase.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="PersistantDatabase.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Common.Mocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PersistantDatabase
+    {
+        private readonly DbContextOptions<MockDbContext> options =
+                MockDbContext.ConfigureInMemoryContextOptions();
+
+        public MockDbContext Create() => new MockDbContext(this.options);
+
+        public List<Entity> CreateEnumeratedEntities(int amount) =>
+            this.CreateEntities(amount, i => CreateEntity(i, $"Name {i}", $"Description {i}"));
+
+        public List<Entity> CreateSimilarEntities(
+            int amount, string name = "Name", string description = "Description") =>
+            this.CreateEntities(amount, i => CreateEntity(i, name, description));
+
+        private static Entity CreateEntity(int id, string name, string description) =>
+            new Entity
+            {
+                Id = id,
+                Name = name,
+                Description = description
+            };
+
+        private List<Entity> CreateEntities(int amount, Func<int, Entity> callback)
+        {
+            using (var context = this.Create())
+            {
+                var entities = Enumerable.Range(1, amount)
+                    .Select(callback)
+                    .ToList();
+                entities.ForEach(e => context.Add(e));
+                context.SaveChanges();
+                return entities;
+            }
+        }
+    }
+}
