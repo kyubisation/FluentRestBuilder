e0406cbc38905f141f361f8382e7cd609b758d19
Lukas Spirig <lukas.spirig@ti8m.ch>
2017-01-18 16:28:30 +0100

    Implemented json mapper

---
+++ src/FluentRestBuilder.Caching/DistributedCache/JsonMapper.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="JsonMapper.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Caching.DistributedCache
+{
+    using Newtonsoft.Json;
+
+    public class JsonMapper<TCacheEntry> : StringMapperBase<TCacheEntry>
+    {
+        protected override string ToString(TCacheEntry cacheEntry) =>
+            JsonConvert.SerializeObject(cacheEntry);
+
+        protected override TCacheEntry FromString(string cacheValue)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<TCacheEntry>(cacheValue);
+            }
+            catch (JsonSerializationException exception)
+            {
+                throw new MappingException(exception.Message, exception);
+            }
+        }
+    }
+}
