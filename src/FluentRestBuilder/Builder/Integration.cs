c1e8c25c6ebffe25ed90bcab0b578d7ceb7a1c9e
Lukas Spirig <lukas.spirig@gmail.com>
2017-02-10 23:44:52 +0100

    Start of documenting the integration methods

--- src/FluentRestBuilder/Builder/Integration.cs
+++ src/FluentRestBuilder/Builder/Integration.cs
@@ -22,12 +22,27 @@ namespace FluentRestBuilder
         public static IFluentRestBuilderCore AddFluentRestBuilderCore(this IServiceCollection collection) =>
             new FluentRestBuilderCore(collection);
 
+        /// <summary>
+        /// Registers the FluentRestBuilder.
+        /// </summary>
+        /// <param name="builder">The MVC builder.</param>
+        /// <returns>The FluentRestBuilder configuration instance.</returns>
         public static IFluentRestBuilder AddFluentRestBuilder(this IMvcBuilder builder) =>
             builder.Services.AddFluentRestBuilder();
 
+        /// <summary>
+        /// Registers the FluentRestBuilder.
+        /// </summary>
+        /// <param name="builder">The MVC core builder.</param>
+        /// <returns>The FluentRestBuilder configuration instance.</returns>
         public static IFluentRestBuilder AddFluentRestBuilder(this IMvcCoreBuilder builder) =>
             builder.Services.AddFluentRestBuilder();
 
+        /// <summary>
+        /// Registers the FluentRestBuilder.
+        /// </summary>
+        /// <param name="collection">The service collection.</param>
+        /// <returns>The FluentRestBuilder configuration instance.</returns>
         public static IFluentRestBuilder AddFluentRestBuilder(this IServiceCollection collection) =>
             new FluentRestBuilder(collection);
 
@@ -37,6 +52,15 @@ namespace FluentRestBuilder
             return builder;
         }
 
+        /// <summary>
+        /// Registers the IHttpContextAccessor to be used as the access for the HttpContext.
+        /// By default the controller extension methods to create the sources use the HttpContext
+        /// provided by the controller.
+        /// This is only necessary if you want to use pipes that require the HttpContext and
+        /// want to create sources without a controller.
+        /// </summary>
+        /// <param name="builder">The FluentRestBuilder configuration instance.</param>
+        /// <returns>The FluentRestBuilder configuration instance.</returns>
         public static IFluentRestBuilder UseHttpContextAccessor(this IFluentRestBuilder builder)
         {
             UseHttpContextAccessor(builder.Services);
