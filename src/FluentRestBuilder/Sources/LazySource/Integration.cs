5f8de7a11c1247069e1470abbbedf6c42a1061c4
Lukas Spirig <lukas.spirig@gmail.com>
2017-02-18 00:22:11 +0100

    Documentation

--- src/FluentRestBuilder/Sources/LazySource/Integration.cs
+++ src/FluentRestBuilder/Sources/LazySource/Integration.cs
@@ -23,11 +23,27 @@ namespace FluentRestBuilder
             return builder;
         }
 
+        /// <summary>
+        /// Create a source pipe with the given asynchronous factory function.
+        /// The factory function is called, when the pipe execution reaches this pipe.
+        /// </summary>
+        /// <typeparam name="TOutput">The output type.</typeparam>
+        /// <param name="controller">The MVC controller.</param>
+        /// <param name="output">The output factory.</param>
+        /// <returns>An output pipe to continue with.</returns>
         public static OutputPipe<TOutput> FromSource<TOutput>(
                 this ControllerBase controller, Func<Task<TOutput>> output) =>
             controller.HttpContext.RequestServices.GetService<ILazySourceFactory<TOutput>>()
                 .Create(output, controller);
 
+        /// <summary>
+        /// Create a source pipe with the given factory function.
+        /// The factory function is called, when the pipe execution reaches this pipe.
+        /// </summary>
+        /// <typeparam name="TOutput">The output type.</typeparam>
+        /// <param name="controller">The MVC controller.</param>
+        /// <param name="output">The output factory.</param>
+        /// <returns>An output pipe to continue with.</returns>
         public static OutputPipe<TOutput> FromSource<TOutput>(
             this ControllerBase controller, Func<TOutput> output) =>
             controller.FromSource(() => Task.FromResult(output()));
--- src/FluentRestBuilder/Sources/Source/Integration.cs
+++ src/FluentRestBuilder/Sources/Source/Integration.cs
@@ -22,11 +22,25 @@ namespace FluentRestBuilder
             return builder;
         }
 
+        /// <summary>
+        /// Create a source pipe with the given value. Awaits the given value.
+        /// </summary>
+        /// <typeparam name="TOutput">The output type.</typeparam>
+        /// <param name="controller">The MVC controller.</param>
+        /// <param name="output">The output value.</param>
+        /// <returns>An output pipe to continue with.</returns>
         public static OutputPipe<TOutput> FromSource<TOutput>(
                 this ControllerBase controller, Task<TOutput> output) =>
             controller.HttpContext.RequestServices.GetService<ISourceFactory<TOutput>>()
                 .Create(output, controller);
 
+        /// <summary>
+        /// Create a source pipe with the given value.
+        /// </summary>
+        /// <typeparam name="TOutput">The output type.</typeparam>
+        /// <param name="controller">The MVC controller.</param>
+        /// <param name="output">The output value.</param>
+        /// <returns>An output pipe to continue with.</returns>
         public static OutputPipe<TOutput> FromSource<TOutput>(
             this ControllerBase controller, TOutput output) =>
             controller.FromSource(Task.FromResult(output));
