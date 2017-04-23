69b880b8167b3d6b1cd7cf809f64d5a438b6514f
Lukas Spirig <lukas.spirig@gmail.com>
2017-04-23 22:13:09 +0200

    Removed global assembly info

--- GlobalAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="GlobalAssemblyInfo.cs" company="Kyubisation">
-// Copyright (c) Kyubisation. All rights reserved.
-// </copyright>
-
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCopyright("Copyright © 2017 FluentRestBuilder")]
-[assembly: ComVisible(false)]
