97e8deb4d65e9dfc927450b56ce180c680b390b7
Lukas Spirig <lukas.spirig@gmail.com>
2017-04-23 14:06:38 +0200

    Fixed pack command file

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,11 @@
-if defined APPVEYOR_REPO_TAG_NAME (
+@echo off
+if not defined APPVEYOR_REPO_TAG_NAME (
+    echo Packaging ci version
+    forfiles /p src /m *.csproj /s /c "cmd /c dotnet pack @file --version-suffix ci-%APPVEYOR_BUILD_NUMBER%"
+) else if "%APPVEYOR_REPO_TAG_NAME%" == "%APPVEYOR_REPO_TAG_NAME:*-=%" (
+    echo Packaging release version
     forfiles /p src /m *.csproj /s /c "cmd /c dotnet pack @file -c Release"
 ) else (
-    forfiles /p src /m *.csproj /s /c "cmd /c dotnet pack @file --version-suffix ci-%APPVEYOR_BUILD_NUMBER%"
+    echo Packaging prerelease %APPVEYOR_REPO_TAG_NAME:*-=%
+    forfiles /p src /m *.csproj /s /c "cmd /c dotnet pack @file -c Release --version-suffix %APPVEYOR_REPO_TAG_NAME:*-=%"
 )
