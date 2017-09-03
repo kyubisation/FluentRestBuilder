75026a177c69f2ca3e53280a7bea1d4558011f40
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-03 12:41:07 +0200

    Implemented async do operator

---
+++ src/FluentRestBuilder/Operators/DoAsyncOperator.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="DoAsyncOperator.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class DoAsyncOperator
+    {
+        /// <summary>
+        /// Asynchronously perform an action on the received value.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="action">The action function.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TSource> DoAsync<TSource>(
+            this IProviderObservable<TSource> observable,
+            Func<TSource, Task> action) =>
+            new DoAsyncObservable<TSource>(action, observable);
+
+        private sealed class DoAsyncObservable<TSource> : Operator<TSource, TSource>
+        {
+            private readonly Func<TSource, Task> action;
+
+            public DoAsyncObservable(
+                Func<TSource, Task> action, IProviderObservable<TSource> observable)
+                : base(observable)
+            {
+                Check.IsNull(action, nameof(action));
+                this.action = action;
+            }
+
+            protected override IObserver<TSource> Create(IObserver<TSource> observer) =>
+                new DoAsyncObserver(this.action, observer, this);
+
+            private sealed class DoAsyncObserver : Observer
+            {
+                private readonly Func<TSource, Task> action;
+                private Task continuationTask;
+
+                public DoAsyncObserver(
+                    Func<TSource, Task> action,
+                    IObserver<TSource> child,
+                    Operator<TSource, TSource> @operator)
+                    : base(child, @operator)
+                {
+                    this.action = action;
+                }
+
+                public override void OnNext(TSource value)
+                {
+                    var task = Task.Run(() => this.action(value));
+                    this.continuationTask = task.ContinueWith(t => this.OnTaskDone(t, value));
+                }
+
+                public override void OnCompleted()
+                {
+                    if (this.continuationTask != null)
+                    {
+                        this.continuationTask.ContinueWith(t => base.OnCompleted());
+                    }
+                    else
+                    {
+                        base.OnCompleted();
+                    }
+                }
+
+                private void OnTaskDone(Task task, TSource value)
+                {
+                    if (task.Exception != null)
+                    {
+                        this.OnError(task.Exception.InnerException);
+                    }
+                    else
+                    {
+                        this.EmitNext(value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/FluentRestBuilder.Test/Operators/DoAsyncOperatorTest.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="DoAsyncOperatorTest.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentRestBuilder.Observables;
+    using FluentRestBuilder.Operators;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class DoAsyncOperatorTest
+    {
+        [Fact]
+        public async Task TestDoAsync()
+        {
+            const string expected = "expected";
+            var value = string.Empty;
+
+            var observable = new SingleObservable<string>(
+                    expected, new ServiceCollection().BuildServiceProvider())
+                .DoAsync(async s =>
+                {
+                    await Task.Delay(100);
+                    value = s;
+                });
+            Assert.Equal(expected, await observable);
+            Assert.Equal(expected, value);
+        }
+
+        [Fact]
+        public async Task TestException()
+        {
+            var observable = new SingleObservable<string>(
+                    string.Empty, new ServiceCollection().BuildServiceProvider())
+                .DoAsync(async s =>
+                {
+                    await Task.Delay(100);
+                    throw new InvalidOperationException();
+                });
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await observable);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var collection = new ServiceCollection();
+            collection.AddTransient<DoAsyncOperatorTest>();
+            var observable = new SingleObservable<string>(
+                    string.Empty, collection.BuildServiceProvider())
+                .DoAsync(async s =>
+                {
+                    await Task.Delay(100);
+                    throw new InvalidOperationException();
+                });
+            var instance = observable.ServiceProvider.GetService<DoAsyncOperatorTest>();
+            Assert.NotNull(instance);
+        }
+    }
+}
