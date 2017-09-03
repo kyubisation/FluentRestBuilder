aa8a63f59911548f64b667ae6dc3ddb57965d060
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-03 12:08:07 +0200

    Implemented async map operator

---
+++ src/FluentRestBuilder/Operators/MapAsyncOperator.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="MapAsyncOperator.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class MapAsyncOperator
+    {
+        /// <summary>
+        /// Asynchronously map the received value to the desired output.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <typeparam name="TTarget">The type of the output.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="mapping">The mapping function.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TTarget> MapAsync<TSource, TTarget>(
+            this IProviderObservable<TSource> observable,
+            Func<TSource, Task<TTarget>> mapping) =>
+            new MapAsyncObservable<TSource, TTarget>(mapping, observable);
+
+        private sealed class MapAsyncObservable<TSource, TTarget> : Operator<TSource, TTarget>
+        {
+            private readonly Func<TSource, Task<TTarget>> mapping;
+
+            public MapAsyncObservable(Func<TSource, Task<TTarget>> mapping, IProviderObservable<TSource> observable)
+                : base(observable)
+            {
+                Check.IsNull(mapping, nameof(mapping));
+                this.mapping = mapping;
+            }
+
+            protected override IObserver<TSource> Create(IObserver<TTarget> observer) =>
+                new MapAsyncObserver(this.mapping, observer, this);
+
+            private sealed class MapAsyncObserver : Observer
+            {
+                private readonly Func<TSource, Task<TTarget>> mapping;
+                private Task continuationTask;
+
+                public MapAsyncObserver(
+                    Func<TSource, Task<TTarget>> mapping,
+                    IObserver<TTarget> child,
+                    Operator<TSource, TTarget> @operator)
+                    : base(child, @operator)
+                {
+                    this.mapping = mapping;
+                }
+
+                public override void OnNext(TSource value)
+                {
+                    var task = Task.Run(() => this.mapping(value));
+                    this.continuationTask = task.ContinueWith(this.OnTaskDone);
+                }
+
+                public override void OnCompleted()
+                {
+                    if (this.continuationTask != null)
+                    {
+                        this.continuationTask.ContinueWith(t => base.OnCompleted());
+                    }
+                    else
+                    {
+                        base.OnCompleted();
+                    }
+                }
+
+                private void OnTaskDone(Task<TTarget> task)
+                {
+                    if (task.Exception != null)
+                    {
+                        this.OnError(task.Exception.InnerException);
+                    }
+                    else
+                    {
+                        this.EmitNext(task.Result);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/FluentRestBuilder.Test/Operators/MapAsyncOperatorTest.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="MapAsyncOperatorTest.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentRestBuilder.Observables;
+    using FluentRestBuilder.Operators;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class MapAsyncOperatorTest
+    {
+        [Fact]
+        public async Task TestMapping()
+        {
+            const string first = "first";
+            const string append = "append";
+            const string expected = first + append;
+
+            var observable = new SingleObservable<string>(
+                    first, new ServiceCollection().BuildServiceProvider())
+                .MapAsync(async s =>
+                {
+                    await Task.Delay(100);
+                    return first + append;
+                });
+            Assert.Equal(expected, await observable);
+        }
+
+        [Fact]
+        public async Task TestException()
+        {
+            var observable = new SingleObservable<string>(
+                    string.Empty, new ServiceCollection().BuildServiceProvider())
+                .MapAsync<string, string>(async s =>
+                {
+                    await Task.Delay(100);
+                    throw new InvalidOperationException();
+                });
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await observable);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var collection = new ServiceCollection();
+            collection.AddTransient<MapAsyncOperatorTest>();
+            var observable = new SingleObservable<string>(
+                    string.Empty, collection.BuildServiceProvider())
+                .MapAsync<string, string>(s => throw new InvalidOperationException());
+            var instance = observable.ServiceProvider.GetService<MapAsyncOperatorTest>();
+            Assert.NotNull(instance);
+        }
+    }
+}
