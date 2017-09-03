3e39b0e0245ae8017f527350450f98aa6e6c5e53
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-03 12:40:39 +0200

    Implemented do operator

---
+++ src/FluentRestBuilder/Operators/DoOperator.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="DoOperator.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Operators
+{
+    using System;
+
+    public static class DoOperator
+    {
+        /// <summary>
+        /// Perform an action on the received value.
+        /// </summary>
+        /// <typeparam name="TFrom">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="action">The action to be performed.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TFrom> Do<TFrom>(
+            this IProviderObservable<TFrom> observable,
+            Action<TFrom> action) =>
+            new DoObservable<TFrom>(action, observable);
+
+        private sealed class DoObservable<TFrom> : Operator<TFrom, TFrom>
+        {
+            private readonly Action<TFrom> action;
+
+            public DoObservable(Action<TFrom> action, IProviderObservable<TFrom> observable)
+                : base(observable)
+            {
+                Check.IsNull(action, nameof(action));
+                this.action = action;
+            }
+
+            protected override IObserver<TFrom> Create(IObserver<TFrom> observer) =>
+                new DoObserver(this.action, observer, this);
+
+            private sealed class DoObserver : Observer
+            {
+                private readonly Action<TFrom> action;
+
+                public DoObserver(
+                    Action<TFrom> action,
+                    IObserver<TFrom> child,
+                    Operator<TFrom, TFrom> @operator)
+                    : base(child, @operator)
+                {
+                    this.action = action;
+                }
+
+                public override void OnNext(TFrom value)
+                {
+                    try
+                    {
+                        this.action(value);
+                        this.EmitNext(value);
+                    }
+                    catch (Exception e)
+                    {
+                        this.OnError(e);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/FluentRestBuilder.Test/Operators/DoOperatorTest.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="DoOperatorTest.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentRestBuilder.Observables;
+    using FluentRestBuilder.Operators;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class DoOperatorTest
+    {
+        [Fact]
+        public async Task TestDo()
+        {
+            const string expected = "expected";
+            var value = string.Empty;
+
+            var observable = new SingleObservable<string>(
+                    expected, new ServiceCollection().BuildServiceProvider())
+                .Do(s => value = s);
+            Assert.Equal(expected, await observable);
+            Assert.Equal(expected, value);
+        }
+
+        [Fact]
+        public async Task TestException()
+        {
+            var observable = new SingleObservable<string>(
+                    string.Empty, new ServiceCollection().BuildServiceProvider())
+                .Do(s => throw new InvalidOperationException());
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => await observable);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var collection = new ServiceCollection();
+            collection.AddTransient<DoOperatorTest>();
+            var observable = new SingleObservable<string>(
+                    string.Empty, collection.BuildServiceProvider())
+                .Do(s => throw new InvalidOperationException());
+            var instance = observable.ServiceProvider.GetService<DoOperatorTest>();
+            Assert.NotNull(instance);
+        }
+    }
+}
