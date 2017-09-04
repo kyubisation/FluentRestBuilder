72d2536f1c69a48d396163ea2bcdf2dea437f486
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-04 22:59:49 +0200

    Implemented async invalid when operator

---
+++ src/FluentRestBuilder/Operators/InvalidWhenAsyncOperator.cs
@@ -0,0 +1,88 @@
+﻿// <copyright file="InvalidWhenAsyncOperator.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+    using Exceptions;
+
+    public static class InvalidWhenAsyncOperator
+    {
+        /// <summary>
+        /// Emits an error if the validation check fails. Otherwise emits the value.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="invalidCheck">The invalidCheck function.</param>
+        /// <param name="statusCode">The status code of the error.</param>
+        /// <param name="errorFactory">The error factory method.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TSource> InvalidWhenAsync<TSource>(
+            this IProviderObservable<TSource> observable,
+            Func<TSource, Task<bool>> invalidCheck,
+            int statusCode,
+            Func<TSource, object> errorFactory = null) =>
+            new InvalidWhenAsyncObservable<TSource>(invalidCheck, statusCode, errorFactory, observable);
+
+        private sealed class InvalidWhenAsyncObservable<TSource> : Operator<TSource, TSource>
+        {
+            private readonly Func<TSource, Task<bool>> invalidCheck;
+            private readonly int statusCode;
+            private readonly Func<TSource, object> errorFactory;
+
+            public InvalidWhenAsyncObservable(
+                Func<TSource, Task<bool>> invalidCheck,
+                int statusCode,
+                Func<TSource, object> errorFactory,
+                IProviderObservable<TSource> observable)
+                : base(observable)
+            {
+                Check.IsNull(invalidCheck, nameof(invalidCheck));
+                this.invalidCheck = invalidCheck;
+                this.statusCode = statusCode;
+                this.errorFactory = errorFactory;
+            }
+
+            protected override IObserver<TSource> Create(
+                IObserver<TSource> observer, IDisposable disposable) =>
+                new InvalidWhenAsyncObserver(
+                    this.invalidCheck, this.statusCode, this.errorFactory, observer, disposable);
+
+            private sealed class InvalidWhenAsyncObserver : SafeAsyncObserver
+            {
+                private readonly Func<TSource, Task<bool>> invalidCheck;
+                private readonly int statusCode;
+                private readonly Func<TSource, object> errorFactory;
+
+                public InvalidWhenAsyncObserver(
+                    Func<TSource, Task<bool>> invalidCheck,
+                    int statusCode,
+                    Func<TSource, object> errorFactory,
+                    IObserver<TSource> child,
+                    IDisposable disposable)
+                    : base(child, disposable)
+                {
+                    this.invalidCheck = invalidCheck;
+                    this.statusCode = statusCode;
+                    this.errorFactory = errorFactory;
+                }
+
+                protected override async Task SafeOnNext(TSource value)
+                {
+                    var isInvalid = await this.invalidCheck(value);
+                    if (isInvalid)
+                    {
+                        var error = this.errorFactory?.Invoke(value);
+                        this.EmitError(new ValidationException(this.statusCode, error));
+                    }
+                    else
+                    {
+                        this.EmitNext(value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/FluentRestBuilder.Test/Operators/InvalidWhenAsyncOperatorTest.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="InvalidWhenAsyncOperatorTest.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Operators
+{
+    using System.Threading.Tasks;
+    using FluentRestBuilder.Observables;
+    using FluentRestBuilder.Operators;
+    using FluentRestBuilder.Operators.Exceptions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Mocks;
+    using Xunit;
+
+    public class InvalidWhenAsyncOperatorTest
+    {
+        [Fact]
+        public async Task TestValidCase()
+        {
+            const string expected = "expected";
+            var observable = new SingleObservable<string>(expected, new EmptyServiceProvider())
+                .InvalidWhenAsync(
+                    async s =>
+                    {
+                        await Task.Delay(100);
+                        return false;
+                    }, 400);
+            Assert.Equal(expected, await observable);
+        }
+
+        [Fact]
+        public async Task TestInvalidCase()
+        {
+            const string expected = "expected";
+            const int statusCode = 400;
+            var observable = new SingleObservable<string>(expected, new EmptyServiceProvider())
+                .InvalidWhenAsync(
+                    async s =>
+                    {
+                        await Task.Delay(100);
+                        return true;
+                    }, statusCode);
+            var exception = await Assert.ThrowsAsync<ValidationException>(async () => await observable);
+            Assert.Equal(statusCode, exception.StatusCode);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var collection = new ServiceCollection();
+            collection.AddTransient<InvalidWhenAsyncOperatorTest>();
+            var observable = new SingleObservable<string>(
+                    string.Empty, collection.BuildServiceProvider())
+                .InvalidWhenAsync(s => Task.FromResult(false), 400);
+            var instance = observable.ServiceProvider.GetService<InvalidWhenAsyncOperatorTest>();
+            Assert.NotNull(instance);
+        }
+    }
+}
