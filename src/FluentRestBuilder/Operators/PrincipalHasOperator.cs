33fd822fec794fb2806f3424ea670b19fc393427
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-11 22:02:58 +0200

    Implemented principal validation

---
+++ src/FluentRestBuilder/Operators/PrincipalHasOperator.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="PrincipalHasOperator.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Operators
+{
+    using System;
+    using System.Security.Claims;
+    using Exceptions;
+    using Filters;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Storage;
+
+    public static class PrincipalHasOperator
+    {
+        /// <summary>
+        /// Emits an error if the principal (the current user) check fails. Otherwise emits the value.
+        /// <para>Requires usage of <see cref="HttpContextProviderAttribute"/>.</para>
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="principalCheck">The principal check function.</param>
+        /// <param name="errorFactory">The error factory method.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TSource> PrincipalHas<TSource>(
+            this IProviderObservable<TSource> observable,
+            Func<ClaimsPrincipal, TSource, bool> principalCheck,
+            Func<TSource, object> errorFactory = null) =>
+            new PrincipalHasObservable<TSource>(principalCheck, errorFactory, observable);
+
+        private sealed class PrincipalHasObservable<TSource> : Operator<TSource, TSource>
+        {
+            private readonly Func<ClaimsPrincipal, TSource, bool> principalCheck;
+            private readonly Func<TSource, object> errorFactory;
+
+            public PrincipalHasObservable(
+                Func<ClaimsPrincipal, TSource, bool> principalCheck,
+                Func<TSource, object> errorFactory,
+                IProviderObservable<TSource> observable)
+                : base(observable)
+            {
+                Check.IsNull(principalCheck, nameof(principalCheck));
+                this.principalCheck = principalCheck;
+                this.errorFactory = errorFactory;
+            }
+
+            protected override IObserver<TSource> Create(
+                IObserver<TSource> observer, IDisposable disposable)
+            {
+                var httpContext = this.ServiceProvider.GetService<IScopedStorage<HttpContext>>();
+                return new PrincipalHasObserver(
+                    httpContext.Value?.User,
+                    this.principalCheck,
+                    this.errorFactory,
+                    observer,
+                    disposable);
+            }
+
+            private sealed class PrincipalHasObserver : SafeObserver
+            {
+                private readonly ClaimsPrincipal principal;
+                private readonly Func<ClaimsPrincipal, TSource, bool> principalCheck;
+                private readonly Func<TSource, object> errorFactory;
+
+                public PrincipalHasObserver(
+                    ClaimsPrincipal principal,
+                    Func<ClaimsPrincipal, TSource, bool> principalCheck,
+                    Func<TSource, object> errorFactory,
+                    IObserver<TSource> child,
+                    IDisposable disposable)
+                    : base(child, disposable)
+                {
+                    Check.IsNull(principal, nameof(principal));
+                    this.principal = principal;
+                    this.principalCheck = principalCheck;
+                    this.errorFactory = errorFactory;
+                }
+
+                protected override void SafeOnNext(TSource value)
+                {
+                    var isValid = this.principalCheck(this.principal, value);
+                    if (isValid)
+                    {
+                        this.EmitNext(value);
+                    }
+                    else
+                    {
+                        var error = this.errorFactory?.Invoke(value);
+                        this.OnError(
+                            new ValidationException(StatusCodes.Status403Forbidden, error));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/FluentRestBuilder.Test/Operators/PrincipalHasAsyncOperatorTest.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="PrincipalHasAsyncOperatorTest.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace FluentRestBuilder.Test.Operators
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentRestBuilder.Observables;
+    using FluentRestBuilder.Operators;
+    using FluentRestBuilder.Operators.Exceptions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Mocks;
+    using Storage;
+    using Xunit;
+
+    public class PrincipalHasAsyncOperatorTest
+    {
+        [Fact]
+        public async Task TestValidCase()
+        {
+            const string expected = "expected";
+            const string claimType = "claim";
+            var collection = new ServiceCollection();
+            collection.AddTransient(
+                s => CreateHttpContextWithPrincipal(
+                    p => p.AddClaim(claimType, expected)));
+            var observable = new SingleObservable<string>(
+                expected, collection.BuildServiceProvider())
+                .PrincipalHasAsync(async (p, i) =>
+                {
+                    await Task.Delay(100);
+                    return p.HasClaim(claimType, expected);
+                });
+            Assert.Equal(expected, await observable);
+        }
+
+        [Fact]
+        public async Task TestInvalidCase()
+        {
+            const string expected = "expected";
+            var collection = new ServiceCollection();
+            collection.AddTransient(s => CreateHttpContextWithPrincipal());
+            var observable = new SingleObservable<string>(
+                expected, collection.BuildServiceProvider())
+                .PrincipalHasAsync(async (p, i) =>
+                {
+                    await Task.Delay(100);
+                    return p.HasClaim("claim", expected);
+                });
+            var exception = await Assert.ThrowsAsync<ValidationException>(
+                async () => await observable);
+            Assert.Equal(StatusCodes.Status403Forbidden, exception.StatusCode);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var collection = new ServiceCollection();
+            collection.AddTransient<PrincipalHasOperatorTest>();
+            var observable = new SingleObservable<string>(
+                    string.Empty, collection.BuildServiceProvider())
+                .PrincipalHasAsync(async (p, i) =>
+                {
+                    await Task.Delay(100);
+                    return p.HasClaim("claim", string.Empty);
+                });
+            var instance = observable.ServiceProvider.GetService<PrincipalHasOperatorTest>();
+            Assert.NotNull(instance);
+        }
+
+        private static IScopedStorage<HttpContext> CreateHttpContextWithPrincipal(
+            Action<MockPrincipal> builder = null)
+        {
+            var principal = new MockPrincipal();
+            builder?.Invoke(principal);
+            return new ScopedStorage<HttpContext>
+            {
+                Value = new DefaultHttpContext
+                {
+                    User = principal,
+                },
+            };
+        }
+    }
+}
