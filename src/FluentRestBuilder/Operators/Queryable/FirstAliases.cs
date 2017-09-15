f3cdd480c5f9a1000180e7d2938b38b1c4508387
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-15 20:21:23 +0200

    Added First and FirstOrDefault aliases

---
+++ src/FluentRestBuilder/Operators/Queryable/FirstAliases.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="FirstAliases.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+// ReSharper disable once CheckNamespace
+namespace FluentRestBuilder
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public static class FirstAliases
+    {
+        /// <summary>
+        /// Emits the first element of a sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TSource> First<TSource>(
+            this IProviderObservable<IQueryable<TSource>> observable) =>
+            observable.Map(s => s.First());
+
+        /// <summary>
+        /// Emits the first element of a sequence that satisfies a specified condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TFrom}"/>.</returns>
+        public static IProviderObservable<TSource> First<TSource>(
+            this IProviderObservable<IQueryable<TSource>> observable,
+            Expression<Func<TSource, bool>> predicate) =>
+            observable.Map(s => s.First(predicate));
+    }
+}
