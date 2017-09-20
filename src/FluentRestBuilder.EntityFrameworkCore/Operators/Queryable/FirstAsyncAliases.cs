7f9744786fc8a14001660293a67bddc15e795811
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-20 18:34:02 +0200

    Implemented queryable aliases

---
+++ src/FluentRestBuilder.EntityFrameworkCore/Operators/Queryable/FirstAsyncAliases.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="FirstAsyncAliases.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+// ReSharper disable once CheckNamespace
+namespace FluentRestBuilder
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class FirstAsyncAliases
+    {
+        /// <summary>
+        /// Emits the first element of a sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TSource}"/>.</returns>
+        public static IProviderObservable<TSource> FirstAsync<TSource>(
+            this IProviderObservable<IQueryable<TSource>> observable) =>
+            observable.MapAsync(async s => await s.FirstAsync());
+
+        /// <summary>
+        /// Emits the first element of a sequence that satisfies a specified condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An instance of <see cref="IProviderObservable{TSource}"/>.</returns>
+        public static IProviderObservable<TSource> FirstAsync<TSource>(
+            this IProviderObservable<IQueryable<TSource>> observable,
+            Expression<Func<TSource, bool>> predicate) =>
+            observable.MapAsync(async s => await s.FirstAsync(predicate));
+    }
+}
---
+++ src/FluentRestBuilder.EntityFrameworkCore/Operators/Queryable/IncludeAlias.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="IncludeAlias.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+// ReSharper disable once CheckNamespace
+namespace FluentRestBuilder
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Query;
+
+    public static class IncludeAlias
+    {
+        /// <summary>
+        /// Specifies related entities to include in the query results. The navigation property
+        /// to be included is specified starting with the type of entity being queried
+        /// (<typeparamref name="TSource" />). If you wish to include additional types based on the
+        /// navigation properties of the type being included, then chain a call to
+        /// <see cref="M:Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.ThenInclude``3(Microsoft.EntityFrameworkCore.Query.IIncludableQueryable{``0,System.Collections.Generic.IEnumerable{``1}},System.Linq.Expressions.Expression{System.Func{``1,``2}})" />
+        /// after this call.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="observable">The parent observable.</param>
+        /// <param name="navigationPropertyPath">
+        /// A lambda expression representing the navigation property to be
+        /// included (<c>t =&gt; t.Property1</c>).
+        /// </param>
+        /// <returns>An instance of <see cref="IProviderObservable{TSource}"/>.</returns>
+        public static IProviderObservable<IIncludableQueryable<TSource, TProperty>> Include<TSource, TProperty>(
+            this IProviderObservable<IQueryable<TSource>> observable,
+            Expression<Func<TSource, TProperty>> navigationPropertyPath)
+            where TSource : class =>
+            observable.Map(s => s.Include(navigationPropertyPath));
+    }
+}
