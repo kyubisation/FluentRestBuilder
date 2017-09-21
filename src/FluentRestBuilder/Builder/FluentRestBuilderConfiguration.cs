6f62c21d0f0b57721986fb85c67466508c8b4b7b
Lukas Spirig <lukas.spirig@gmail.com>
2017-09-21 21:12:15 +0200

    Removed obsolete pipe

--- src/FluentRestBuilder/Builder/FluentRestBuilderConfiguration.cs
+++ src/FluentRestBuilder/Builder/FluentRestBuilderConfiguration.cs
@@ -14,7 +14,6 @@ namespace FluentRestBuilder.Builder
             new FluentRestBuilderCoreConfiguration(this.Services)
                 .RegisterSource()
                 .RegisterLazySource()
-                .RegisterActionPipe()
                 .RegisterFilterByClientRequestPipe()
                 .RegisterMappingPipe()
                 .RegisterOrderByClientRequestPipe()
