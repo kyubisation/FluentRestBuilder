763d585f26e29bffaf70942b3b4653d2d4d5c329
Lukas Spirig <lukas.spirig@gmail.com>
2017-10-18 22:57:47 +0200

    Created tool to generate operator documentation

---
+++ tools/OperatorsToRst/Templating/RstTemplate.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="RstTemplate.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace OperatorsToRst.Templating
+{
+    using System.IO;
+    using System.Reflection;
+    using DotLiquid;
+    using DotLiquid.NamingConventions;
+
+    public class RstTemplate
+    {
+        private readonly Template template;
+
+        static RstTemplate()
+        {
+            Template.NamingConvention = new CSharpNamingConvention();
+            Template.RegisterFilter(typeof(TextFilters));
+        }
+
+        public RstTemplate(string template)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var embeddedFile = $"{nameof(OperatorsToRst)}.{template}.csrst";
+            using (var stream = assembly.GetManifestResourceStream(embeddedFile))
+            using (var reader = new StreamReader(stream))
+            {
+                this.template = Template.Parse(reader.ReadToEnd());
+            }
+        }
+
+        public string Render<TValue>(TValue value) =>
+            this.template.Render(Hash.FromAnonymousObject(value));
+    }
+}
---
+++ tools/OperatorsToRst/Templating/TextFilters.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="TextFilters.cs" company="Kyubisation">
+// Copyright (c) Kyubisation. All rights reserved.
+// </copyright>
+
+namespace OperatorsToRst.Templating
+{
+    public static class TextFilters
+    {
+        public static string WrapSignature(string signature)
+        {
+            var index = signature.IndexOf('(');
+            return signature.Insert(index + 1, "\n" + new string(' ', 8));
+        }
+    }
+}
